9bb625bac230896c0d9a972f8c1702552ba7ddb4
oshuster <godzilasanya@gmail.com>
2024-02-06 06:37:14 +0200

    remove redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,43 +1,12 @@
-import { combineReducers } from '@reduxjs/toolkit';
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from '../redux/contacts/contactsSlice';
 import { filterReducer } from '../redux/filter/filterSlice';
 
-import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['contacts'],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
 });
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export default store;
