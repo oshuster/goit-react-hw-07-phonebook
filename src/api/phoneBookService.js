14618575272e8e31c8b2a3ab4d92aa191fb00a06
oshuster <godzilasanya@gmail.com>
2024-02-06 07:08:32 +0200

    add api description

---
+++ src/api/phoneBookService.js
@@ -0,0 +1,26 @@
+// POST for create contact
+// https://65c1b7e4dc74300bce8dd570.mockapi.io/contacts
+// example body
+// {
+//   "createdAt": "06022024-07-02",
+//   "name": "Test Test",
+//   "phone": "380948732342"
+// }
+
+//GET for get all contacts
+//https://65c1b7e4dc74300bce8dd570.mockapi.io/contacts
+
+//GET for get contact by id
+//https://65c1b7e4dc74300bce8dd570.mockapi.io/contacts/:id
+
+//DELETE for delete contact by id
+//https://65c1b7e4dc74300bce8dd570.mockapi.io/contacts
+
+// fetchContacts - одержання масиву контактів(метод GET) запитом.
+// Базовий тип екшену "contacts/fetchAll".
+
+// addContact - додавання контакту(метод POST).
+// Базовий тип екшену "contacts/addContact".
+
+// deleteContact - видалення контакту(метод DELETE).
+// Базовий тип екшену "contacts/deleteContact".
