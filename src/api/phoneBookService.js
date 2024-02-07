126f6624dca4ad47543457d0948e2626a4c8e174
oshuster <godzilasanya@gmail.com>
2024-02-07 06:49:52 +0200

    add contacts api

--- src/api/phoneBookService.js
+++ src/api/phoneBookService.js
@@ -1,26 +1,18 @@
-// POST for create contact
-// https://65c1b7e4dc74300bce8dd570.mockapi.io/contacts
-// example body
-// {
-//   "createdAt": "06022024-07-02",
-//   "name": "Test Test",
-//   "phone": "380948732342"
-// }
+import axios from 'axios';
 
-//GET for get all contacts
-//https://65c1b7e4dc74300bce8dd570.mockapi.io/contacts
+const instance = axios.create({
+  baseURL: 'https://65c1b7e4dc74300bce8dd570.mockapi.io/contacts',
+  timeout: 30000,
+});
 
-//GET for get contact by id
-//https://65c1b7e4dc74300bce8dd570.mockapi.io/contacts/:id
+export const getContacts = () => {
+  return instance.get('/');
+};
 
-//DELETE for delete contact by id
-//https://65c1b7e4dc74300bce8dd570.mockapi.io/contacts
+export const delContacts = id => {
+  return instance.delete(`/${id}`);
+};
 
-// fetchContacts - одержання масиву контактів(метод GET) запитом.
-// Базовий тип екшену "contacts/fetchAll".
-
-// addContact - додавання контакту(метод POST).
-// Базовий тип екшену "contacts/addContact".
-
-// deleteContact - видалення контакту(метод DELETE).
-// Базовий тип екшену "contacts/deleteContact".
+export const addContact = body => {
+  return instance.post('/', body);
+};
